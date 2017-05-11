38c75bd07029eb39cc05937742f2304ce14a3abb
jiang818@qq.com <263088049>
2017-05-11 23:15:35 +0800

    优化了简单的自动路由

--- likephp/core/Route.php
+++ likephp/core/Route.php
@@ -14,15 +14,18 @@ namespace likephp\core;
 class Route
 {
 
-	public $module;
-	public $controller;
-	public $action;
+	public $app;
+	public $ctrl;
+	public $act;
+
+	private $_pathinfo;
 
 	function __construct()
 	{
 		$var_pathinfo = Config::get('var_pathinfo');
 		if (isset($_GET[$var_pathinfo])) {
 			$_SERVER['PATH_INFO'] = $_GET[$var_pathinfo];
+			unset($_GET[$var_pathinfo]);
 		}
 		if (!isset($_SERVER['PATH_INFO'])) {
 			foreach (Config::get('pathinfo_fetch') as $type) {
@@ -33,15 +36,122 @@ class Route
 				}
 			}
 		}
+		$this->_pathinfo();
+		$this->app = $this->_get_app();
+		$this->ctrl = $this->_get_ctrl();
+		$this->act = $this->_get_act();
+		$this->_setGet();
+
+	}
+
+	private function _pathinfo()
+	{
 		if (!empty($_SERVER['PATH_INFO'])) {
-			$pathinfo_array = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+			$trim_path = trim($_SERVER['PATH_INFO']);
+			if (strpos($trim_path, '.')) {
+				$pathinfo_all = explode('.', $trim_path);
+				$pathinfo = $pathinfo_all[0];
+			} else {
+				$pathinfo = $trim_path;
+			}
+			$this->_pathinfo = explode('/', trim($pathinfo, '/'));
 		}
-		$module = isset($pathinfo_array[0]) ? $pathinfo_array[0] : Config::get('default_module');
-		$controller = isset($pathinfo_array[1]) ? $pathinfo_array[1] : Config::get('default_controller');
-		$action = isset($pathinfo_array[2]) ? $pathinfo_array[2] : Config::get('default_action');
-		$this->module = strtolower($module);
-		$this->controller = ucfirst(strtolower($controller));
-		$this->action = $action;
+	}
+
+	/**
+	 * 获取应用
+	 * User: jiangxijun
+	 * Email: jiang818@qq.com
+	 * Qq: 263088049
+	 * @return string
+	 */
+	private function _get_app()
+	{
+		$default_app = Config::get('default_app', 'index');
+		if (defined('BIND_APP')) {
+			$app = BIND_APP;
+		} else if (isset($this->_pathinfo[0])) {
+			$app = $this->_pathinfo[0];
+			unset($this->_pathinfo[0]);
+		} else {
+			$app = $default_app;
+		}
+		return strtolower($app);
+	}
+
+	/**
+	 * 获取控制器
+	 * User: jiangxijun
+	 * Email: jiang818@qq.com
+	 * Qq: 263088049
+	 * @return string
+	 */
+	private function _get_ctrl()
+	{
+		$default_ctrl = Config::get('default_ctrl', 'index');
+		if (defined('BIND_CTRL')) {
+			$ctrl = BIND_CTRL;
+		} else if (isset($this->_pathinfo[0]) && defined('BIND_APP')) {
+			$ctrl = $this->_pathinfo[0];
+			unset($this->_pathinfo[0]);
+		} else if (isset($this->_pathinfo[1])) {
+			$ctrl = $this->_pathinfo[1];
+			unset($this->_pathinfo[1]);
+		} else {
+			$ctrl = $default_ctrl;
+		}
+		return ucfirst(strtolower($ctrl));
+	}
+
+	/**
+	 * 获取操作
+	 * User: jiangxijun
+	 * Email: jiang818@qq.com
+	 * Qq: 263088049
+	 * @return bool|null|string
+	 */
+	private function _get_act()
+	{
+		$default_act = Config::get('default_act', 'index');
+		if (defined('BIND_ACT')) {
+			$act = BIND_ACT;
+		} else if (isset($this->_pathinfo[0]) && defined('BIND_APP') && defined('BIND_CTRL')) {
+			$act = $this->_pathinfo[0];
+			unset($this->_pathinfo[0]);
+		} else if (isset($this->_pathinfo[1]) && defined('BIND_APP')) {
+			$act = $this->_pathinfo[1];
+			unset($this->_pathinfo[1]);
+		} else if (isset($this->_pathinfo[2])) {
+			$act = $this->_pathinfo[2];
+			unset($this->_pathinfo[2]);
+		} else {
+			$act = $default_act;
+		}
+		return $act;
+	}
+
+	/**
+	 * 设置get参数
+	 * User: jiangxijun
+	 * Email: jiang818@qq.com
+	 * Qq: 263088049
+	 */
+	private function _setGet()
+	{
+		if (false !== $this->_pathinfo) {
+			if (isset($_GET[$_SERVER['QUERY_STRING']])) {
+				unset($_GET[$_SERVER['QUERY_STRING']]);
+			}
+			$params_array = array_values($this->_pathinfo);
+			$count = count($params_array);
+			$i = 0;
+			while ($i < $count) {
+				if (isset($params_array[$i + 1])) {
+					$_GET[$params_array[$i]] = $params_array[$i + 1];
+				}
+				$i += 2;
+			}
+		}
 	}
 }
 
