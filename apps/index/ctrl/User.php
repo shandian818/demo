1a6d9f0737643a1903d3c0bf8d3f85539a3713df
jiang818@qq.com <263088049>
2017-05-11 23:15:01 +0800

    add

---
+++ apps/index/ctrl/User.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiangxijun
+ * Email: jiang818@qq.com
+ * Qq: 263088049
+ * Date: 2017/5/11
+ * Time: 21:31
+ */
+
+namespace apps\index\ctrl;
+
+
+class User
+{
+	public function index()
+	{
+		echo 'user-index';
+	}
+
+	public function list()
+	{
+		echo 'user-list';
+	}
+
+	public function add()
+	{
+		echo 'user-add';
+	}
+}
