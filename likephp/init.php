48b8fb36c63c85399aefb5a9ba20fea0f25fb11d
jiangxijun <jiang818@qq.com>
2017-05-25 17:55:30 +0800

    dev

--- likephp/init.php
+++ likephp/init.php
@@ -12,6 +12,7 @@ namespace likephp;
 
 define('LIKE_VER', '0.0.1');
 define('DS', DIRECTORY_SEPARATOR);
+defined('APP_DEBUG') or define("APP_DEBUG", false);//默认调试模式关闭
 defined('APPS_PATH') or define('APPS_PATH', realpath(dirname($_SERVER['SCRIPT_FILENAME'])) . DS);
 define("ROOT_PATH", realpath(dirname(APPS_PATH)) . DS);
 defined('LIKE_PATH') or define("LIKE_PATH", realpath(__DIR__) . DS);
