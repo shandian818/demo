4fd6975307f04e4f885ba4097f7cb0e6d1676c97
jiangxijun <jiang818@qq.com>
2017-08-23 17:16:47 +0800

    dev

---
+++ apps/index/model/UserModel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jiangxijun
+ * Email: jiang818@qq.com
+ * Qq: 263088049
+ * Date: 2017/8/23
+ * Time: 16:31
+ */
+
+namespace apps\index\model;
+
+
+use likephp\core\Model;
+
+class UserModel extends Model
+{
+
+}
