6fd6c035fb3dd3fd81064bee53654cd73ad33e8a
jiangxijun <jiang818@qq.com>
2017-08-29 17:45:54 +0800

    dev

--- apps/index/ctrl/Index.php
+++ apps/index/ctrl/Index.php
@@ -13,6 +13,7 @@ namespace apps\index\ctrl;
 
 use apps\index\model\UserModel;
 use likephp\core\Ctrl;
+use likephp\core\Model;
 use Medoo\Medoo;
 
 class Index extends Ctrl
@@ -42,10 +43,19 @@ class Index extends Ctrl
 //		dump($database->last());
 
 		$user_model = new UserModel();
-		dump($user_model);
-//		$list = $user_model->where('uid>1')->field('uid,uname,unick')->select();
+//		dump($user_model);
+		$list = $user_model->where('uid>1')->field('uid,uname,unick')->group('status')->order('uid DESC')->select();
 //		$list = $user_model->where([])->field('a,b,c')->select();
-//		dump($list);
+		dump($list);
+		dump($user_model);
+		dump($user_model->getLastSql());
+
+//
+		$model = new Model();
+		$list = $model->table('User')->where('uid>1')->field('uid,uname,unick')->group('status')->order('uid DESC')->comment('测试注释')->select();
+		dump($list);
+		dump($model);
+		dump($model->getLastSql());
 	}
 
 	public function test()
